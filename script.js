a88c9bff60dde6bb4929b1c5a8d98b8e16b57ab5
unknown <sugeppe@gmail.com>
2024-08-10 16:15:50 +0200

    eighth commit

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const game = () => {
     let moves = 0;
  
  
+
     // Function to 
     const playGame = () => {
         const sassoBtn = document.querySelector('.sasso');
@@ -22,22 +23,22 @@ const game = () => {
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerText = `Scelta nr.: ${moves}`;
- 
- 
+                
                 const choiceNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[choiceNumber];
+                const result = document.querySelector('.pcchoice')
+                result.textContent = (computerChoice)
                 
- 
                 // Function to check who wins
                 winner(this.innerText, computerChoice)
  
-                // Calling gameOver function after 10 moves
+                // Calling gameOver function  first 5 scores
                 if (playerScore == 5 || computerScore==5) {
                     gameOver(playerOptions, movesLeft);
                 }
             })
         })
- 
+       
     }
  
     // Function to decide winner
@@ -48,7 +49,7 @@ const game = () => {
         player = player.toLowerCase();
         computer = computer.toLowerCase();
         if (player === computer) {
-            result.textContent = 'Patta'
+            result.textContent = 'Round pareggiato'
         }
         else if (player == 'sasso') {
             if (computer == 'carta') {
@@ -102,21 +103,21 @@ const game = () => {
         movesLeft.style.display = 'none';
  
         if (playerScore > computerScore) {
-            result.style.fontSize = '2rem';
-            result.innerText = 'Hai vinto la partita !!'
-            result.style.color = '#308D46';
+            result.style.fontSize = '1rem';
+            result.innerText = 'Hai VINTO la partita !!'
+            result.style.color = 'blue';
         }
         else if (playerScore < computerScore) {
-            result.style.fontSize = '2rem';
-            result.innerText = 'Hai perso !!';
-            result.style.color = 'white';
+            result.style.fontSize = '1rem';
+            result.innerText = 'Hai PERSO la partita !!';
+            result.style.color = 'red';
         }
         else {
             result.style.fontSize = '2rem';
             result.innerText = 'Tie';
             result.style.color = 'white'
         }
-        reloadBtn.innerText = 'Restart';
+        reloadBtn.innerText = 'Gioca ancora';
         reloadBtn.style.display = 'flex'
         reloadBtn.addEventListener('click', () => {
             window.location.reload();
